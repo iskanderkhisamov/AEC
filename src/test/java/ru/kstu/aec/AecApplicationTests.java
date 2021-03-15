d03bebf74947af47d0855188f40db251fcdf10a0
”Iskander <iskanderkhisamov@gmail.com>
2021-03-15 15:54:50 +0300

    init spring

---
+++ src/test/java/ru/kstu/aec/AecApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kstu.aec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
