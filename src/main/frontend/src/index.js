c3229351ce311190a02ca1d7f61091e2df9b40b8
”Iskander <iskanderkhisamov@gmail.com>
2021-04-03 18:39:59 +0300

    Добавил графики

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -4,10 +4,8 @@ import reportWebVitals from './reportWebVitals';
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
