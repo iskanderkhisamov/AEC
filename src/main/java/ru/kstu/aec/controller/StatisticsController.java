5757c120559b183c85b04ebc318c60632d69dd87
Hanekawa-chan <captainduck.official@gmail.com>
2021-04-03 18:58:37 +0300

    my changes

--- src/main/java/ru/kstu/aec/controller/StatisticsController.java
+++ src/main/java/ru/kstu/aec/controller/StatisticsController.java
@@ -1,5 +1,6 @@
 package ru.kstu.aec.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class StatisticsController {
 
     @GetMapping("/statistics")
-    public String statistics() {
-        return "index";
+    public String handleGet() {
+        return "redir";
     }
 }
