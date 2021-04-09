fbf63bc8e774317a4e5c042115c095f70ffec78c
Hanekawa-chan <captainduck.official@gmail.com>
2021-04-10 00:39:34 +0300

    фикс

--- src/main/java/ru/kstu/aec/controller/IndexController.java
+++ src/main/java/ru/kstu/aec/controller/IndexController.java
@@ -3,15 +3,20 @@ package ru.kstu.aec.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.kstu.aec.models.User;
+import ru.kstu.aec.models.UserRole;
 
 @Controller
 public class IndexController {
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("auth", true);
         System.out.println(getAuthentication());
+        System.out.println(getAuthentication().getAuthorities().toArray()[0]);
+        System.out.println(getAuthentication().getAuthorities().toArray()[0].equals("true"));
         return "index";
     }
 
