9131b63cb2fb358ddfb184be12cca0fb20ff7d2c
”Iskander <iskanderkhisamov@gmail.com>
2021-04-30 23:03:14 +0300

    Updated header

--- src/main/java/ru/kstu/aec/controller/TestsController.java
+++ src/main/java/ru/kstu/aec/controller/TestsController.java
@@ -1,14 +1,23 @@
 package ru.kstu.aec.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.kstu.aec.models.User;
+
+import static ru.kstu.aec.configs.SecurityConfig.getAuthentication;
+import static ru.kstu.aec.configs.SecurityConfig.isTeacher;
 
 @Controller
 public class TestsController {
 
     @GetMapping("/tests")
-    public String tests() {
+    public String tests(Model model) {
+        isTeacher(model);
+        model.addAttribute("name", ((User) getAuthentication().getPrincipal()).getName());
+        model.addAttribute("surname", ((User) getAuthentication().getPrincipal()).getSurname());
+        model.addAttribute("group", ((User) getAuthentication().getPrincipal()).getGruppa());
         return "tests";
     }
 
